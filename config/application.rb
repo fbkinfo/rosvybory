5aa76c2b7c5e48f03dc124fc87eb76d9cf2c765c
biomancer <biomancer@gmail.com>
2013-07-26 15:09:51 +0400

    Локаль

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,10 @@ module Rosvibory
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Moscow'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ru
   end
 end
