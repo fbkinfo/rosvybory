975ebdb0b3ad92c4565e63015f6c457ac354bcb5
root <root@d1stkfactory>
2013-07-26 09:53:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rosvibory::Application.config.secret_key_base = '340d3971f807b012796d41d7855108b49041c507fb46a1bdbe79045d2bd0cfa1ee581a4ac71f60cc7af0308a5f233d2fa7d692d53adf263b1d211daf0f748ca7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rosvibory::Application.config.session_store :cookie_store, key: '_rosvibory_session'
