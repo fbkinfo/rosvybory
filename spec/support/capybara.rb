21a7dcf1457d7f3419765e2805a23d4415d194f6
Yury Omelchuk <jurgen@kodolabs.com>
2013-08-14 11:50:51 +0300

    added capybara gem, cleaned up Gemfile

---
+++ spec/support/capybara.rb
@@ -0,0 +1,8 @@
+require 'capybara/rspec'
+
+#Capybara.configure do |config|
+#  config.match = :one
+#  config.exact_options = true
+#  config.ignore_hidden_elements = true
+#  config.visible_text_only = true
+#end
