230ee24e92d5b5fc8d4f80d6015eac2b1cc1facf
Nick Recobra <oruenu@gmail.com>
2013-08-15 01:18:57 +0400

    Не надо тут пользователя создавать.

--- db/migrate/20130727132534_devise_create_admin_users.rb
+++ db/migrate/20130727132534_devise_create_admin_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    User.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
+    #User.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
   end
 
   def change
