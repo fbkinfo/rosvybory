c22300da4028b68c7818bcf02eeec20422ffa4ab
Nick Recobra <oruenu@gmail.com>
2013-08-15 01:57:12 +0400

    Дополнен конфиг юникорна.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,40 @@
 shared = File.expand_path(File.join(File.dirname(__FILE__), '../../../shared'))
 
+env = ENV["RAILS_ENV"]
+
+if env == "production"
+  RAILS_ROOT = "/home/dev/production/rosvybory/current"
+  user 'dev', 'dev'
+else
+  RAILS_ROOT = "/home/ubuntu/staging/rosvybory/current"
+  user 'ubuntu', 'ubuntu'
+end
+
+working_directory RAILS_ROOT
+
+if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
+  begin
+    rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+    rvm_lib_path = File.join(rvm_path, 'lib')
+    $LOAD_PATH.unshift rvm_lib_path
+    require 'rvm'
+    RVM.use_from_path! RAILS_ROOT
+  rescue LoadError
+    raise "RVM ruby lib is currently unavailable."
+  end
+end
+
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+require 'bundler/setup'
+
 worker_processes 2
 
 timeout 30
 
 preload_app true
+
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
 
 pid File.join(shared, 'pids/unicorn.pid')
 
