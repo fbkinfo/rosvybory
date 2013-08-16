fa551b466b26657d9bb79cbbbbd51953b0030524
Nick Recobra <oruenu@gmail.com>
2013-08-16 18:55:38 +0400

    Русская локаль — основная.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Rosvibory
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
   end
 end
