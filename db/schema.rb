4589de1856a005998c38be18fe36595642682a31
Дьячук Антон <antiqe@gmail.com>
2013-08-17 01:42:59 +0600

    миграция тестов

--- db/schema.rb
+++ db/schema.rb
@@ -107,8 +107,8 @@ ActiveRecord::Schema.define(version: 20130816160517) do
     t.string   "state",              default: "pending", null: false
     t.boolean  "phone_verified",     default: false,     null: false
     t.boolean  "has_video"
-    t.string   "forwarded_for"
     t.integer  "organisation_id"
+    t.string   "forwarded_for"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
   end
