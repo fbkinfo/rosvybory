c24c37c215edfbb4b9cada42cf459f7c4a8f0091
Nick Recobra <oruenu@gmail.com>
2013-08-17 02:08:54 +0400

    Конфиг рекаптчи.

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LeMMuYSAAAAAA9rYZ7oCz2vtbKlt_kwx_Uyn2JC'
+  config.private_key = '6LeMMuYSAAAAAFwIaryP0prfXObRDMZBo-PiUMm-'
+  #config.proxy = 'http://myproxy.com.au:8080'
+end
