4b0ec6a677c16790881e0caf18ca8ea177a39d1a
Дьячук Антон <antiqe@gmail.com>
2013-08-17 02:07:31 +0600

    default_url_options for production and staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -78,6 +78,10 @@ Rosvibory::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = {
+    :host => "lhttp://staging.rosvybory.org/"
+  }
+
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
