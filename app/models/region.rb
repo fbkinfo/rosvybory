9d08c30e0d4c723acd4998d2d460dc462fdd8bf5
Kir Shatrov <shatrov@me.com>
2013-08-17 15:21:40 +0400

    Исправления после правок для enumerize

--- app/models/region.rb
+++ app/models/region.rb
@@ -4,15 +4,14 @@ class Region < ActiveRecord::Base
   belongs_to :parent, class_name: 'Region'
   validates_uniqueness_of :name
 
-  enumerize :kind, in: {city: 1, adm_region: 2, mun_region: 3}, default: :city
+  enumerize :kind, in: {city: 1, adm_region: 2, mun_region: 3}, default: :city, scope: true
 
-  scope :cities, -> { where(kind: CITY).order :name }
-  scope :adm_regions, -> { where(kind: ADM_REGION)}
-  scope :mun_regions, -> { where(kind: MUN_REGION).order :name }
+  scope :cities, -> { with_kind(:city).order :name }
+  scope :adm_regions, -> { with_kind(:adm_region) }
+  scope :mun_regions, -> { with_kind(:mun_region).order(:name) }
   scope :with_tics, -> { where(has_tic: true) }
 
   has_many :regions, -> { order :name }, foreign_key: "parent_id"
-
 
   def subregions_with_tics
     if has_tic?
@@ -21,5 +20,4 @@ class Region < ActiveRecord::Base
       regions.with_tics
     end
   end
-
 end
