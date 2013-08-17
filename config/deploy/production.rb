d6f78e706aed91a44219f1c04aeea3df42c628c3
Nick Recobra <oruenu@gmail.com>
2013-08-17 04:12:54 +0400

    Управление демоном очереди в продакшне.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,15 +10,20 @@ set :rails_env, 'production'
 namespace :deploy do
   task :start do
     run "sudo /etc/init.d/unicorn_init start rosvybory_production"
+    run "sudo /etc/init.d/rosvybory start"
   end
 
   task :stop do
     run "sudo /etc/init.d/unicorn_init stop rosvybory_production"
+    run "sudo /etc/init.d/rosvybory stop"
   end
 
   task :restart do
     run "sudo /etc/init.d/unicorn_init stop rosvybory_production"
     run "sudo /etc/init.d/unicorn_init start rosvybory_production"
+
+    run "sudo /etc/init.d/rosvybory stop"
+    run "sudo /etc/init.d/rosvybory start"
   end
 end
 
