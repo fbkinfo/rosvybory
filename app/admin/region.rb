75e61aa6168927c859eaf6765eb66cfe16571104
Kir Shatrov <shatrov@me.com>
2013-08-17 14:33:32 +0400

    Enumerize для регионов. Переводы типов

--- app/admin/region.rb
+++ app/admin/region.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Region do
 
+  index do
+    column :id
+    column :kind do |region|
+      region.kind.text
+    end
+
+    column :name
+    column :has_tic
+    actions
+  end
+
 end
