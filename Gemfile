946c5c563e06d9f27e91dd36ffaa51ce72ca5f65
Kir Shatrov <shatrov@me.com>
2013-08-17 14:33:32 +0400

    Enumerize для регионов. Переводы типов

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'foreman-export-initscript', require: false
 gem 'activeadmin',         github: 'gregbell/active_admin', branch: 'rails4'
 gem 'ransack', "~> 1.0.0"
 gem 'inherited_resources', "~> 1.4.1"
+gem 'enumerize'
 gem 'sms_ru'
 
 group :development do
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,14 +1,15 @@
 class Region < ActiveRecord::Base
+  extend Enumerize
 
   belongs_to :parent, class_name: 'Region'
   validates_uniqueness_of :name
 
-  CITY, ADM_REGION, MUN_REGION = 1, 2, 3
+  enumerize :kind, in: {city: 1, adm_region: 2, mun_region: 3}, default: :city
 
   scope :cities, -> { where(kind: CITY).order :name }
   scope :adm_regions, -> { where(kind: ADM_REGION)}
   scope :mun_regions, -> { where(kind: MUN_REGION).order :name }
-  scope :with_tics, -> { where(:has_tic => true) }
+  scope :with_tics, -> { where(has_tic: true) }
 
   has_many :regions, -> { order :name }, foreign_key: "parent_id"
 
