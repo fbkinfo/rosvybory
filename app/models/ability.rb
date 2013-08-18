ee2de177c8f6464b6a311d858ac927532a870532
Peter Adrianov <peter.adrianov@gmail.com>
2013-08-18 00:51:08 -0700

    Замена user.region.id на user.region_id

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,11 +20,11 @@ class Ability
 
     if has_role?(user, :tc)
       #ТК видит заявки своего адм. округа или района и только из своего НО
-      can :manage, UserApp, :region_id => user.region.id, :organisation_id => user.organisation_id
-      can :manage, User, :region_id => user.region.id, :organisation_id => user.organisation_id
+      can :manage, UserApp, :region_id => user.region_id, :organisation_id => user.organisation_id
+      can :manage, User, :region_id => user.region_id, :organisation_id => user.organisation_id
 
       if user.region.kind.adm_region?
-        can :manage, UserApp, :adm_region_id => user.region.id, :organisation_id => user.organisation_id
+        can :manage, UserApp, :adm_region_id => user.region_id, :organisation_id => user.organisation_id
         can :manage, User, :region => { :parent_id => user.region_id }, :organisation_id => user.organisation_id
       end
     end
