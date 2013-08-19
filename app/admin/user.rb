31d82669761621c4c1681addd99b7da205034535
biomancer <biomancer@gmail.com>
2013-08-19 16:19:55 +0400

    Немного рефакторинга и если нет заявки, то данные не отображаются совсем(вместо Нет и 0)

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -34,51 +34,47 @@ ActiveAdmin.register User do
     column :adm_region
     column :region
     column "№ УИК" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).uic : ''
+      user.user_app.try(:decorate).try(:uic)
     end
     column "Готов стать" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).desired_statuses : ''
+      user.user_app.try(:decorate).try(:desired_statuses)
     end
     column "Пол" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).sex_male : ''
+      user.user_app.try(:decorate).try(:sex_male)
     end
     column "Текущие статусы" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).current_roles : ''
+      user.user_app.try(:decorate).try(:current_roles)
     end
     column "Прежний опыт: количество раз" do |user|
-      user.user_app ? user.user_app.experience_count : 0
+      user.user_app.try(:experience_count)
     end
     column "Прежний опыт: статусы" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).previous_statuses : ''
+      user.user_app.try(:decorate).try(:previous_statuses)
     end
     column "Может быть ТК" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).can_be_coord_region : ''
+      user.user_app.try(:decorate).try(:can_be_coord_region)
     end
     column "Автомобиль" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).has_car : ''
+      user.user_app.try(:decorate).try(:has_car)
     end
     column "Видеосъёмка" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).has_video : ''
+      user.user_app.try(:decorate).try(:has_video)
     end
     column "Юр.образование" do |user|
       if user.user_app
         user.user_app.legal_status & UserApp::LEGAL_STATUS_YES ? "Да":"Нет"
-      else
-        'Нет'
       end
     end
     column "Адвокатский статус" do |user|
       if user.user_app
         user.user_app.legal_status & UserApp::LEGAL_STATUS_LAWYER ? "Да":"Нет"
-      else
-        'Нет'
       end
     end
     column "Соцсети" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).social_accounts : ''
+      user.user_app.try(:decorate).try(:social_accounts)
     end
     column "Дополнительные сведения" do |user|
-      user.user_app ? UserAppDecorator.decorate(user.user_app).extra : ''
+      user.user_app.try(:decorate).try(:extra)
     end
 
     default_actions
