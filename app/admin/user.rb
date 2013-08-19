6325621336db730dc207b9c9c258b53c6d77a15c
Peter M. Adrianov <peter.adrianov@gmail.com>
2013-08-19 15:20:40 +0400

    Вывод пользователей в полном формате.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -20,14 +20,65 @@ ActiveAdmin.register User do
     end
   end if Role.table_exists?
 
+  #TODO: Нужен рефакторинг
   index do
-    column :email
+    column :created_at
+    column "ФИО" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).full_name : ''
+    end
     column :phone
-    column :current_sign_in_at
-    column :last_sign_in_at
-    column :sign_in_count
-    column :region
+    column :email
     column :organisation
+    column :adm_region
+    column :region
+    column "№ УИК" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).uic : ''
+    end
+    column "Готов стать" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).desired_statuses : ''
+    end
+    column "Пол" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).sex_male : ''
+    end
+    column "Текущие статусы" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).current_roles : ''
+    end
+    column "Прежний опыт: количество раз" do |user|
+      user.user_app ? user.user_app.experience_count : 0
+    end
+    column "Прежний опыт: статусы" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).previous_statuses : ''
+    end
+    column "Может быть ТК" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).can_be_coord_region : ''
+    end
+    column "Автомобиль" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).has_car : ''
+    end
+    column "Видеосъёмка" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).has_video : ''
+    end
+    column "Юр.образование" do |user|
+      if user.user_app
+        user.user_app.legal_status & UserApp::LEGAL_STATUS_YES ? "Да":"Нет"
+      else
+        'Нет'
+      end
+    end
+    column "Адвокатский статус" do |user|
+      if user.user_app
+        user.user_app.legal_status & UserApp::LEGAL_STATUS_LAWYER ? "Да":"Нет"
+      else
+        'Нет'
+      end
+    end
+    column "Соцсети" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).social_accounts : ''
+    end
+    column "Дополнительные сведения" do |user|
+      user.user_app ? UserAppDecorator.decorate(user.user_app).extra : ''
+    end
+
     default_actions
   end
 
