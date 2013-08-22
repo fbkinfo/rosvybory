a7c034749ce19a7b28e240a19796ad21269e5cc2
biomancer <biomancer@gmail.com>
2013-08-22 22:00:22 +0400

    Поправлено отображение пользователей в табличной форме

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,8 +1,8 @@
 class UserDecorator < Draper::Decorator
   delegate_all
-  delegate :can_be_caller, :can_be_coord_region, :can_be_mobile, :current_roles, :extra,
-           :experience_count, :full_name, :has_car, :has_video, :legal_status,
-           :previous_statuses, :social_accounts, :uic,
+  delegate :can_be_caller, :can_be_coord_region, :can_be_mobile, :human_current_roles, :extra,
+           :experience_count, :full_name, :human_has_car, :human_has_video, :human_legal_status,
+           :human_previous_statuses, :human_social_accounts, :uic,
             to: :decorated_user_app, allow_nil: true
 
   def user_current_roles
@@ -16,5 +16,11 @@ class UserDecorator < Draper::Decorator
   def decorated_user_app
     user_app.try(:decorate)
   end
+
+  def human_roles
+    object.roles.pluck(:short_name).join("; ")
+  end
+
+
 
 end
