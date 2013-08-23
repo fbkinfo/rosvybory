dbda71dc7c2e1ee239495fa7abf3e73c0eced383
Vladimir Meremyanin <vladimir@meremyanin.com>
2013-08-23 22:36:38 +0400

    removed test code from production

--- app/controllers/sms_passwords_controller.rb
+++
@@ -1,7 +0,0 @@
-class SmsPasswordsController < ActiveAdmin::Devise::PasswordsController
-
-  protected
-    def after_sending_reset_password_instructions_path_for(resource)
-      raise super.inspect
-    end
-end
