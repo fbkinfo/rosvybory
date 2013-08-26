7473d59f11ce9bcb99cda005c026f95ff263b425
AlexanderPavlenko <alerticus@gmail.com>
2013-08-26 14:03:19 +0400

    УИК у наблюдателя

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130823202746) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20130826082649) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
