1d63ca769c759bcb26bbc1f37eec40634b887025
Vladimir Meremyanin <vladimir@meremyanin.com>
2013-08-26 20:31:41 +0400

    added comment about Dislocation.with_current_roles

--- app/models/dislocation.rb
+++ app/models/dislocation.rb
@@ -8,6 +8,8 @@ class Dislocation < User
     Arel::Nodes::SqlLiteral.new("nomination_source_id")
   end
 
+  # возвращает пользователей и их текущие роли (один пользователь может быть 1 и больше раз)
+  # каждая строка содержит все поля пользователя + uic_id, current_role_id и nomination_source_id из user_current_roles
   def self.with_current_roles
     joins("left join user_current_roles on user_current_roles.user_id = users.id").
       select("users.*, uic_id as current_role_uic_id, current_role_id as current_role_id, nomination_source_id as current_role_nomination_source_id")
