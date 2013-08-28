4cea3755b3334584a47d09710ab9b80b9b62f0e8
AlexanderPavlenko <alerticus@gmail.com>
2013-08-28 17:12:18 +0400

    Добавлена роль "Представитель СМИ" и тип источника выдвижения "СМИ"

--- app/models/current_role.rb
+++ app/models/current_role.rb
@@ -1,7 +1,18 @@
 class CurrentRole < ActiveRecord::Base
+  KNOWN_VARIANTS = %w(
+    journalist
+    observer
+    prg
+    prg_tic
+    psg
+    psg_tic
+    reserve
+  )
 
   has_many :user_app_current_roles, dependent: :destroy
   has_many :user_apps, through: :user_app_current_roles
+
+  validates :slug, presence: true, inclusion: {in: KNOWN_VARIANTS}
 
   default_scope -> { order(CurrentRole.arel_table[:position].asc) }
 
