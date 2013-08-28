6a715f85ec67e6c363d1e96b2fc1c076a958f0c6
biomancer <biomancer@gmail.com>
2013-08-29 00:38:34 +0400

    typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   include UserAppsHelper
 
   before_filter :expose_current_roles,
-      only: [:new, :edit, :group_new :dislocate]
+      only: [:new, :edit, :group_new, :dislocate]
   before_filter :set_user, only: [:edit, :update, :dislocate]
 
   def dislocate
