236fb6cbc987f1de882ea847e6c1cad84c80ac18
Alex Kukushkin <alex@kukushk.in>
2013-08-29 23:47:34 +0300

    Признак "Документы выданы" перенесен из пользователей в расстановки.
    - поле users.got_docs переехало в user_current_roles.got_docs
    - убраны фильтр и колонка "Документы выданы" из пользователей
    - добавлен фильтр по признаку в расстановки
    - поправлены формы редактирования пользователя и ролей на участке

--- app/models/dislocation.rb
+++ app/models/dislocation.rb
@@ -8,11 +8,20 @@ class Dislocation < User
     Arel::Nodes::SqlLiteral.new("nomination_source_id")
   end
 
+  ransacker :got_docs do
+    UserCurrentRole.arel_table[:got_docs]
+  end
+
   # возвращает пользователей и их текущие роли (один пользователь может быть 1 и больше раз)
   # каждая строка содержит все поля пользователя + uic_id, current_role_id и nomination_source_id из user_current_roles
   def self.with_current_roles
     joins("left join user_current_roles on user_current_roles.user_id = users.id").
-      select("users.*, uic_id as current_role_uic_id, current_role_id as current_role_id, nomination_source_id as current_role_nomination_source_id")
+      select(
+        "users.*, uic_id as current_role_uic_id, "+
+        "current_role_id as current_role_id, "+
+        "nomination_source_id as current_role_nomination_source_id, "+
+        "got_docs"
+      )
   end
 
   def self.with_role(role_slug)
@@ -21,6 +30,6 @@ class Dislocation < User
 
 
   def self.ransackable_attributes(auth_object = nil)
-    column_names + ["current_role_uic", 'current_role_nomination_source_id']
+    column_names + ["current_role_uic", 'current_role_nomination_source_id', 'got_docs']
   end
 end
