900587f5cdaaa2a5839e5b46e713ddf18b5f2c94
Alex Kukushkin <alex@kukushk.in>
2013-08-30 23:42:46 +0300

    Исправлен доступ к "Контролю расстановок" для ФП и ТК
    - разрешен доступ :read для Uic для всех (ресурс DislocationControl это производный от Uic)
    - добавлен вид доступа :view_user_contacts, выдан  ФП и ТК

--- app/decorators/dislocation_control_decorator.rb
+++ app/decorators/dislocation_control_decorator.rb
@@ -1,7 +1,7 @@
 class DislocationControlDecorator < Draper::Decorator
   delegate_all
 
-  def human_participant(index)
+  def human_participant(index, show_contacts = false)
     role = participant(index)
     return unless role
     user = role.user
@@ -13,12 +13,15 @@ class DislocationControlDecorator < Draper::Decorator
         source.name,
         source.human_variant
       ].join(', ') || 'Не указан',
-      user.phone,
-      h.link_to(user.email, "mailto:#{user.email}"),
-    ].join('<br/>').html_safe
+      show_contacts && [
+        user.phone,
+        h.link_to(user.email, "mailto:#{user.email}")
+      ] || '***'  #  'контакты скрыты'
+    ].flatten.join('<br/>').html_safe
   end
 
   def number_and_region
     [number, region.name].join(', ')
   end
+
 end
