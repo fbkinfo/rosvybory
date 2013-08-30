7f02db37bbc3a96a9cb55ad2754eb51075255cf9
Alex Kukushkin <alex@kukushk.in>
2013-08-30 05:04:39 +0300

    Изменение функционала проверки корректности расстановки:
    - добавлены настраиваемые правила Dislocation::DISLOCATION_RULES
    - партия кандидата не может назначать ПСГ на УИК

--- app/models/dislocation.rb
+++ app/models/dislocation.rb
@@ -1,7 +1,8 @@
 class Dislocation < User
 
-  # slug ролей, которые учитываются при проверке корректности расстановки
-  CHECK_DISLOCATION_CURRENT_ROLES = [ 'observer', 'psg' ]
+  belongs_to :nomination_source, foreign_key: :user_current_role_nomination_source_id
+  belongs_to :current_role, foreign_key: :user_current_role_current_role_id
+  belongs_to :uic, foreign_key: :user_current_role_uic_id
 
   ransacker :current_role_uic do
     Arel::Nodes::SqlLiteral.new("(select number from uics where uics.id = uic_id)")
@@ -30,42 +31,72 @@ class Dislocation < User
   end
 
 
+  # DISLOCATION_RULES:
+  # Ограничения расстановки, в зависимости от типа источника выдвижения.
+  # каждое правило имеет вид:
+  #  <тип_источника> => { <ограничения> }
+  # ограничения это Hash следующего вида:
+  #  <роль_наблюдателя> => <количество>
+  #
+  # Расстановка некорректна, если количество наблюдателей данного типа от одного источника
+  # превысило ограничение для этого типа наблюдателя.
+  #
+  # Если правило для типа источника не существует — ограничений для этого типа источника нет.
+  # Если в правиле для типа источника не указано ограничение для роли наблюдателя — ограничений
+  #   для этой роли наблюдателя нет.
+  #
+  DISLOCATION_RULES = {
+    'candidate'  => { 'psg' => 1 },
+    'party'      => { 'observer' => 1, 'psg' => 0 },
+    'parliament' => { 'observer' => 1, 'psg' => 1 }
+  }
+
+  # Контролируемые роли наблюдателя
+  CONTROLLED_ROLES_SLUGS = DISLOCATION_RULES.values.map(&:keys).flatten.uniq
+
   # Проверяет корректность расстановки.
   # Возвращает +Array+ с сообщениями об ошибках, или +nil+ если ошибок не найдено
   #
   # Расстановка некорректна, если хотя бы одно условие выполняется:
   # 1) на этом же участке этот же наблюдатель назначен более одного раза
-  # 2) на этом же участке, в этой же роли, от того же источника, расставлено более одного наблюдателя
+  #    на роли из набора контролируемых ролей
+  # 2) для источника выдвижения и типа наблюдателя нарушаются DISLOCATION_RULES
   #
   def check_dislocation_for_errors
-    check_current_role_ids = CurrentRole.where( slug: CHECK_DISLOCATION_CURRENT_ROLES ).pluck(:id)
-    unless check_current_role_ids.include? current_role_id
+    current_role_id = user_current_role_current_role_id
+    # собираем все контролируемые роли
+    @controlled_roles_ids ||= CurrentRole.where( slug: CONTROLLED_ROLES_SLUGS ).pluck(:id)
+    unless @controlled_roles_ids.include? current_role_id
       # текущая роль не входит в проверяемые
       return nil
     end
 
-    # все расстановки на этом же участке с ролями из CHECK_DISLOCATION_CURRENT_ROLES
-    uic_ucr = UserCurrentRole.where( uic_id: current_role_uic_id, current_role_id: check_current_role_ids )
+    # все расстановки на этом же участке с контролируемыми ролями
+    uic_ucr = UserCurrentRole.where( uic_id: user_current_role_uic_id, current_role_id: @controlled_roles_ids )
     errors = nil
 
     # 1) сколько раз пользователь расставлен на участок
     check_user_count = uic_ucr.where( user_id: id ).count
     if check_user_count > 1
-      uic_number = Uic.find( current_role_uic_id ).try(:number)
       errors ||= []
-      errors << "наблюдатель расставлен на УИК № #{uic_number} больше одного раза"
+      errors << "наблюдатель расставлен на УИК № #{uic.try(:number)} больше одного раза"
     end
 
     # 2) сколько таких же ролей от того же источника выдвижения
+    return nil unless nomination_source.present?
+    return nil unless DISLOCATION_RULES[nomination_source.variant].present?
+    dislocation_rule_limit = DISLOCATION_RULES[nomination_source.variant][current_role.slug]
+    return nil unless dislocation_rule_limit.present?
+
     check_role_nomination_count = uic_ucr.where(
       current_role_id: current_role_id,
-      nomination_source_id: current_role_nomination_source_id
+      nomination_source_id: user_current_role_nomination_source_id
     ).count
-    if check_role_nomination_count > 1
-      current_role_name = CurrentRole.find( current_role_id ).try(:name)
-      nomination_source_name = NominationSource.find( current_role_nomination_source_id ).try(:name)
+    if check_role_nomination_count > dislocation_rule_limit
       errors ||= []
-      errors << "на УИК № #{uic_number} расставлено больше одного '#{current_role_name}' от источника '#{nomination_source_name}'"
+      errors << "источник '#{nomination_source.name}' типа '#{nomination_source.human_variant}'"+
+        " не может назначить больше #{dislocation_rule_limit} наблюдателей "+
+        " в роли '#{current_role.try(:name)}' на УИК № #{uic.try(:number)}."
     end
     errors
   end
@@ -73,6 +104,6 @@ class Dislocation < User
   # Уникальный идентификатор расстановки.
   #
   def duid
-    "duid_#{id}_#{current_role_uic_id}_#{current_role_id}_#{current_role_nomination_source_id}"
+    "duid_#{id}_#{user_current_role_uic_id}_#{user_current_role_id}_#{user_current_role_nomination_source_id}"
   end
 end
