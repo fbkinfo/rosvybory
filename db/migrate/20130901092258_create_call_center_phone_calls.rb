e4584ed9ed40058b7ddeae9517a68215aa47b093
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-01 16:01:32 +0400

    Заполняем таблицы с типами и категориями нарушений данными, у звонка появилась внешняя связь на оператора.

--- db/migrate/20130901092258_create_call_center_phone_calls.rb
+++ db/migrate/20130901092258_create_call_center_phone_calls.rb
@@ -3,7 +3,7 @@ class CreateCallCenterPhoneCalls < ActiveRecord::Migration
     create_table :call_center_phone_calls do |t|
       t.string :status
       t.string :number
-
+      t.references :call_center_operator
       t.timestamps
     end
   end
