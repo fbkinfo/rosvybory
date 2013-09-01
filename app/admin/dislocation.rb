657f667609469f77ba682ba4b9bae54dc43fec0d
Данила <danila.sinopalnikov@gmail.com>
2013-09-01 10:49:04 +0000

    Support for 'show all' option

--- app/admin/dislocation.rb
+++ app/admin/dislocation.rb
@@ -117,9 +117,20 @@ ActiveAdmin.register Dislocation do
     render :json => results
   end
 
+  # TODO(sinopalnikov): move common code to app/admin/concerns
+  action_item(only: [:index]) do
+    _show_all = params[:show_all] && params[:show_all].to_sym == :true
+    _label = I18n.t('views.pagination.actions.pagination_' + (_show_all ? 'on' : 'off'))
+    link_to _label, control_users_path(:format => nil, :show_all => (_show_all ? :false : :true))
+  end
+
   controller do
     def scoped_collection
       Dislocation.with_current_roles.with_role :observer
+    end
+
+    def apply_pagination(chain)
+      return super.per(params[:show_all] && params[:show_all].to_sym == :true ? 1000000 : nil)
     end
   end
 
