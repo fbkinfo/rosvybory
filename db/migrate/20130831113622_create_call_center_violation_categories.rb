ce0b0a8ba51906a1f60d461e8e52aaa3120932d3
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-01 14:53:17 +0400

    add migrations for call center.

--- db/migrate/20130831113622_create_call_center_violation_categories.rb
+++ db/migrate/20130831113622_create_call_center_violation_categories.rb
@@ -4,6 +4,7 @@ class CreateCallCenterViolationCategories < ActiveRecord::Migration
   def change
     create_table :call_center_violation_categories do |t|
       t.string :name
+
       t.timestamps
     end
 
