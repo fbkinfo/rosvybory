937eb7d39f661065df65846ecec61858b728e7b6
Vladimir Meremyanin <vladimir@meremyanin.com>
2013-09-02 08:25:42 +0400

    added todo

--- app/admin/shared_show_all.rb
+++ app/admin/shared_show_all.rb
@@ -1,4 +1,6 @@
 ActiveAdmin::Event.subscribe ActiveAdmin::Resource::RegisterEvent do |resource|
+
+  # TODO move this into PaginatedCollection rendering
   resource.add_action_item(only: [:index]) do
     _show_all = params[:show_all] && params[:show_all].to_sym == :true
     _label = I18n.t('views.pagination.actions.pagination_' + (_show_all ? 'on' : 'off'))
@@ -7,7 +9,7 @@ ActiveAdmin::Event.subscribe ActiveAdmin::Resource::RegisterEvent do |resource|
 
   resource.controller.class_eval do
     def apply_pagination(chain)
-      return super.per(params[:show_all] && params[:show_all].to_sym == :true ? 1000000 : nil)
+      super.per(params[:show_all] && params[:show_all].to_sym == :true ? 1000000 : nil)
     end
   end
 end
