723fb6cebd604bc341cb0e340c8f52f4b5012034
Anton Aljoshin <Anton_Aleschin@mail.ru>
2013-09-02 18:26:59 +0400

    CallCenter. Chore. Removed debug code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  rescue_from Exception do |exception|
-    logger.error exception.class
-    logger.error exception.message
-    logger.error exception.backtrace.to_s
-  end
 end
