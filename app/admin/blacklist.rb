8c62067dbda3aaa4ab577d90550685fc9b0060bc
Vladimir Meremyanin <vladimir@meremyanin.com>
2013-09-02 17:24:23 +0400

    nest blacklist under dictionaries in menu

--- app/admin/blacklist.rb
+++ app/admin/blacklist.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Blacklist do
-  menu :if => proc{ can? :manage, Blacklist }
+  menu :parent => I18n.t('active_admin.menu.dictionaries'), :if => proc{ can? :manage, Blacklist }
 
   actions :all, except: [:show, :edit]
   batch_action :destroy, false
