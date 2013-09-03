c3ac4e8362d285a6395d422401cbf3551c121686
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-03 15:23:17 +0400

    add locales for models. remove shit.

--- app/admin/call_center/violation_category.rb
+++ app/admin/call_center/violation_category.rb
@@ -1,15 +1,4 @@
 ActiveAdmin.register CallCenter::ViolationCategory do
   menu parent: I18n.t('active_admin.menu.call_center')
   actions :all
-
-  # index do
-  #  [:id, :name].each do |col|
-  #     column col
-  #   end
-  #   column :violation_category do |violation_type|
-  #     violation_type.violation_category.name
-  #   end
-  #   default_actions
-  # end
-
 end
