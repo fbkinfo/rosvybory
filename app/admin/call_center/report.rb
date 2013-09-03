ae9f6f4a4936c62c365df7880f4a047c0ed0e48a
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-03 14:58:01 +0400

    add calls.

--- app/admin/call_center/report.rb
+++ app/admin/call_center/report.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register CallCenter::Report do
-  menu parent: I18n.t('active_admin.menu.call_center'), label: 'Сообщения'
+  menu parent: I18n.t('active_admin.menu.call_center')
   actions :all
 
   index do
