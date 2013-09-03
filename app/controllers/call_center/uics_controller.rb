369c0a50fe25831c678f193aad16dee733bdd80d
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-03 17:23:32 +0400

    CallCenter. universal search. https://www.pivotaltracker.com/story/show/56259200

--- app/controllers/call_center/uics_controller.rb
+++ app/controllers/call_center/uics_controller.rb
@@ -2,7 +2,7 @@ class CallCenter::UicsController < ApplicationController
   respond_to :json, :html
 
   def index
-    @uics = Uic.where(number: params[:q])
+    @uics = Uic.where("name like :q", q: params[:q].mb_chars.upcase)
     respond_with @uics
   end
 
