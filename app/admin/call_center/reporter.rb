95d1e9bae00da3f85597559ffad36f1a5cb21d1f
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-03 15:23:17 +0400

    add locales for models. remove shit.

--- app/admin/call_center/reporter.rb
+++ app/admin/call_center/reporter.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register CallCenter::Reporter do
-  menu parent: I18n.t('active_admin.menu.call_center'), label: 'Звонки'
+  menu parent: I18n.t('active_admin.menu.call_center')
   actions :all
 end
