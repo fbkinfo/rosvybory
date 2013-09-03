06e9168954477ac48824d15085891e76f2dcc696
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-03 14:16:23 +0400

    add menu for reports.

---
+++ app/admin/call_center/report.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register CallCenter::Report do
+  menu parent: I18n.t('active_admin.menu.call_center'), label: 'Сообщения'
+  actions :all
+
+  index do
+    column :id
+    column :text
+    column :url do |report|
+      link_to(report.send(:url), report.url) unless report.url.empty?
+    end
+    column :reporter do |report|
+      r = report.reporter
+      [r.last_name, r.first_name, r.patronymic].join ' '
+    end
+    column :created_at
+    column :updated_at
+    default_actions
+  end
+
+end
