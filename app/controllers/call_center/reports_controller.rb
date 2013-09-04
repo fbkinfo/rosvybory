eb182a91b7a76e185f196cab8890feb4ef92f798
Anton Aljoshin <Anton_Aleschin@mail.ru>
2013-09-04 16:41:37 +0400

    CallCenter. Привязывание PhoneCall к Report'у. Отказался от полиморфной ассоциации у Report и заменил её на violation.

--- app/controllers/call_center/reports_controller.rb
+++ app/controllers/call_center/reports_controller.rb
@@ -3,7 +3,11 @@ class CallCenter::ReportsController < ApplicationController
 
   def new
     @dislocation = Dislocation.find_by phone: params[:phone]
-    @report = CallCenter::Report.new reporter: new_reporter_from(@dislocation)
+    @report = CallCenter::Report.new\
+      reporter: new_reporter_from(@dislocation),
+      phone_call: CallCenter::PhoneCall.create(number: params[:phone], status: "started"),
+      violation: CallCenter::Violation.new
+
     @uic = @report.reporter.try(:uic)
   end
 
@@ -14,17 +18,25 @@ class CallCenter::ReportsController < ApplicationController
         reporter.first_name = reporter.dislocation.first_name
         reporter.last_name = reporter.dislocation.last_name
         reporter.patronymic = reporter.dislocation.patronymic
+        reporter.mobile_group = reporter.dislocation.mobile_group
+        reporter.adm_region = reporter.dislocation.adm_region
       end
     end
     report.save
-    
+
+    CallCenter::PhoneCall.find(params[:phone_call_id]).tap do |phone_call|
+      phone_call.report = report
+      phone_call.status = "completed"
+      phone_call.save
+    end
+
     redirect_to new_call_center_report_path
   end
 
   private
 
   def permitted_params
-    params.require(:call_center_report).permit :parent_report_ids, reporter_attributes: [:phone, :uic, :user_id, :role, :uic_id, :current_role_id, :last_name, :first_name, :patronymic, {report: [:text, violation: [:violation_type]]}]
+    params.require(:call_center_report).permit :text, {violation_attributes: [:violation_type_id]}, :parent_report_ids, reporter_attributes: [:phone, :uic, :user_id, :role, :uic_id, :current_role_id, :last_name, :first_name, :patronymic]
   end
 
   def new_reporter_from(dislocation)
@@ -36,6 +48,8 @@ class CallCenter::ReportsController < ApplicationController
         reporter.last_name  = dislocation.last_name
         reporter.patronymic = dislocation.patronymic
         reporter.current_role = dislocation.user_current_roles.first.current_role
+        reporter.mobile_group = dislocation.mobile_group
+        reporter.adm_region = dislocation.adm_region
       end
     end
   end
