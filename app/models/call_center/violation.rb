d47a647b297c6848f4e40c8dd2237f3d281fb25e
Lisovsky Vlad <lisovskyvlad@gmail.com>
2013-09-05 16:32:46 +0400

    call_center. initial implemantation of json load.

--- app/models/call_center/violation.rb
+++ app/models/call_center/violation.rb
@@ -1,4 +1,17 @@
 class CallCenter::Violation < ActiveRecord::Base
   belongs_to :violation_type
   has_one :report
+
+  def to_json
+    text = report.text rescue ''
+    uic_name = report.reporter.uic.name rescue ''
+    { violation_type: violation_type.try(:name), uic: uic_name, text: text, created_at: created_at }.to_json
+  end
+
+  class << self
+    def create_json_load
+      path = File.join('public/', 'violations.json')
+      File.open(path, "w") { |f| f.write(all.map(&:to_json).join("\n"))  }
+    end
+  end
 end
