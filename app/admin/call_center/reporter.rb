344700cc7936394980119ef80f1e0bd93ab7d9b1
Anton Aljoshin <Anton_Aleschin@mail.ru>
2013-09-05 15:51:25 +0400

    CallCenter. Права доступа. Ограничил доступ до панели оператора до пользователей с ролью "оператор колл-центра" (:callcenter).

--- app/admin/call_center/reporter.rb
+++ app/admin/call_center/reporter.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register CallCenter::Reporter do
-  menu parent: I18n.t('active_admin.menu.call_center'), priority: 45, if: proc{ can? :read,  CallCenter::Reporter }
+  menu parent: I18n.t('active_admin.menu.call_center'), priority: 5, if: proc{ can? :read,  CallCenter::Reporter }
 
   scope "Зарегистрированные" do |items|
     items.where "user_id IS NOT NULL"
