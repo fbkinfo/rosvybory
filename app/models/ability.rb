52ee0e3e18bddf1829765930a1767f106900a1ae
biomancer <biomancer@gmail.com>
2013-09-05 18:05:38 +0400

    Поправлены ошибки в синтаксисе в abilities.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -123,11 +123,11 @@ class Ability
       can [:create, :read], ActiveAdmin::Comment
     end
 
-    if user.has_role?(user, :callcenter)
+    if user.has_role?(:callcenter)
       can :create, CallCenter::Report
     end
 
-    if user.has_role?(user, :cc)
+    if user.has_role?(:cc)
       # TODO КК может просматривать только участников КЦ в координаторском формате и формате "Состав КЦ".
       can [:create, :read], ActiveAdmin::Comment
       CallCenter.constants.each { |m| can(:crud, "CallCenter::#{m}".constantize) }
