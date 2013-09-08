a5df898f2c63b7e644b7c7e600670dc36cbfeb12
Vladimir Meremyanin <vladimir@meremyanin.com>
2013-09-08 20:35:11 +0400

    queue sms in batches

--- app/services/sms_mass_sender.rb
+++ app/services/sms_mass_sender.rb
@@ -1,6 +1,19 @@
 class SmsMassSender
   @queue = :mailer
 
+  def self.spam(current_user, phones, message)
+    phones.in_groups_of(500).each do |pg|
+      params = {
+        phones: pg,
+        message: message
+      }
+      worklog = WorkLog.create  :user_id => current_user.id,
+                                :name => 'Sending SMS',
+                                :params => options.to_json
+      Resque.enqueue(SmsMassSender, options)
+    end
+  end
+
   def self.perform(*args)
     params = args[0]
 
