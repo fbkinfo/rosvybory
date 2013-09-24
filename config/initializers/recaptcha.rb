0263e5e91e92a0cdbd3276ef5390ae6c091bdfc6
biomancer <biomancer@gmail.com>
2013-09-24 22:17:06 +0400

    Ключи Recaptha вынесены в конфиг

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Lc7OOYSAAAAAMYrtJUBBrMHBMVF507AhW1xwdO9'
-  config.private_key = '6Lc7OOYSAAAAABXxGkHG-AyiAjdzaodQijxcUTqa'
+  config.public_key  = AppConfig["captcha_public_key"]
+  config.private_key = AppConfig["captcha_private_key"]
   config.handle_timeouts_gracefully = false
   config.skip_verify_env << "development"
   #config.proxy = 'http://myproxy.com.au:8080'
