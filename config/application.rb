bc3c8dbdbec61554eaaf18d8b2f18f355421d83e
biomancer <biomancer@gmail.com>
2013-09-25 01:15:49 +0400

    Поправлен путь к конфигу

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 # load config
-AppConfig = YAML.load_file(Rails.root.join('config', 'config.yml'))
+AppConfig = YAML.load_file('config/config.yml')
 # Override config options by correct environment
 env_options = AppConfig.delete(Rails.env)
 AppConfig.merge!(env_options) unless env_options.nil?
