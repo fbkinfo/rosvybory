2de78adee628abe2f907b1867b8049e60582641a
biomancer <biomancer@gmail.com>
2013-09-24 22:06:25 +0400

    Secret key base вынесен в конфиг

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Rosvibory::Application.config.secret_key_base = '87ee38a23fd00d17a377cb10d95e5791579db061a4a91254e91d761f600fd1a271304fb11bca35274c52d6744c996512377aa7c558d319a8911fe06538a127e4'
+Rosvibory::Application.config.secret_key_base = AppConfig["secret_key_base"]
